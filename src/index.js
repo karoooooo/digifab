0f502ff2094b9027326734fc52657f8a6d898e96
Karoline <kanojo94@gmail.com>
2022-10-02 21:49:59 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -8,26 +8,3 @@ function hideMenu (){
  }
 }
 
-// function noPage (){
-//   const showMessage = alert("Denne siden finnes ikke")
-
-// }
-
-/* 
-
-General: Nav, if it doesn't have an a-attribute, hide it
-
-MOBILE: 
-
-clicked nav hamburger-menu
-sliding nav from the side, moving "main" down
-
-DESKTOP:
-hamburger-menu hidden
-
-hover nav: menu items appearing 
-
-"id=about" clicked in desktop
-redirected to about in main (opening it)
-
-*/
